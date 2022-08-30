26beb7b51f685f5323f7c1c5b2fe53a9c9b0dd86
Melvin Darvieux <darvieuxmelvin@gmail.com>
2022-08-30 15:43:17 +0200

    temporarily finished controller view

--- app/controllers/user_actions_controller.rb
+++ app/controllers/user_actions_controller.rb
@@ -1,12 +1,13 @@
 class UserActionsController < ApplicationController
   def index
-    @transport_actions = UserAction.where({ category: 'transport' })
-    @food_actions = UserAction.where({ category: 'food' })
-    @digital_actions = UserAction.where({ category: 'digital' })
-    @household_actions = UserAction.where({ category: 'household' })
+    @transport_actions = UserAction.joins(:action).where({ category: 'transport' })
+    @food_actions = UserAction.joins(:action).where({ category: 'food' })
+    @digital_actions = UserAction.joins(:action).where({ category: 'digital' })
+    @household_actions = UserAction.joins(:action).where({ category: 'household' })
+    @user_infos = current_user
   end
 
   def show
-    @action = UserAction.find(params[:id])
+    @action = UserAction.joins(:action).find(params[:id])
   end
 end
