3b758a73ca38f8bd97999f36fa1db6fa92ca52a0
Melvin Darvieux <darvieuxmelvin@gmail.com>
2022-08-30 14:59:30 +0200

    migrations added + controller view finished for index and show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_100458) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_122607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_100458) do
     t.bigint "action_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
+    t.string "title"
     t.index ["action_id"], name: "index_user_actions_on_action_id"
     t.index ["user_id"], name: "index_user_actions_on_user_id"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,7 @@ user1 = User.create!({email: 'jean.jordan@gmail.com', password: '123456', first_
 
   action2 = Action.create({ title: 'household question', level: 'beginner', score: '7', category: 'household', description: "do some stuff for the planet", occurences: 1 })
 
-  action3 = Action.create({ title: 'numeric question', level: 'beginner', score: '7', category: 'numeric', description: "do some stuff for the planet", occurences: 1 })
+  action3 = Action.create({ title: 'numeric question', level: 'beginner', score: '7', category: 'digital', description: "do some stuff for the planet", occurences: 1 })
 
   action4 = Action.create({ title: 'food question', level: 'beginner', score: '7', category: 'food', description: "do some stuff for the planet", occurences: 1 })
 
@@ -30,10 +30,39 @@ user1 = User.create!({email: 'jean.jordan@gmail.com', password: '123456', first_
 #######################    USER-ACTIONS ###############################################################
 
 
-  user_action1 = UserAction.create({ status: 'unvalidated', user_occurences: 0, user_id: user1.id, action_id: action1.id, score: action1.score })
+  user_action1 = UserAction.create({
+                                      title: 'transport question',
+                                      status: 'unvalidated',
+                                      user_occurences: 0,
+                                      user_id: user1.id,
+                                      action_id: action1.id,
+                                      score: action1.score,
+                                      category: action1.category
+                                    })
 
-  user_action2 = UserAction.create({ status: 'unvalidated', user_occurences: 0, user_id: user1.id, action_id: action2.id, score: action2.score })
+  user_action2 = UserAction.create({
+                                      title: 'household question',
+                                      status: 'unvalidated',
+                                      user_occurences: 0,
+                                      user_id: user1.id,
+                                      action_id: action2.id,
+                                      score: action2.score,
+                                      category: action2.category })
 
-  user_action3 = UserAction.create({ status: 'unvalidated', user_occurences: 0, user_id: user1.id, action_id: action4.id, score: action3.score })
+  user_action3 = UserAction.create({
+                                      title: 'digital question',
+                                      status: 'unvalidated',
+                                      user_occurences: 0,
+                                      user_id: user1.id,
+                                      action_id: action3.id,
+                                      score: action3.score,
+                                      category: action3.category })
 
-  user_action4 = UserAction.create({ status: 'unvalidated', user_occurences: 0, user_id: user1.id, action_id: action4.id, score: action4.score })
+  user_action4 = UserAction.create({
+                                      title: 'food question',
+                                      status: 'unvalidated',
+                                      user_occurences: 0,
+                                      user_id: user1.id,
+                                      action_id: action4.id,
+                                      score: action4.score,
+                                      category: action4.category })
