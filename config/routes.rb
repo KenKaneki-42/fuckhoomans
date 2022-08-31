a388235af4caacf2e011d5645b5ab0fadf2f0cdf
Kenkaneki-42 <sylvain.vandermeersch@gmail.com>
2022-08-31 17:59:46 +0200

    add pages components + route dédiée

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "pages#home"
-  get "about", to: "pages#about"
-  get "contact", to: "pages#contact"
+  get "component", to: "pages#component"
+  # get "contact", to: "pages#contact"
   resources :user_actions, only: %w[index show]
   resources :actions, only: %w[index show]
 end
