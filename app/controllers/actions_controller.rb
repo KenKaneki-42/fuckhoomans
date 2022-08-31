94fb9f21f40698a1dc8358b3b0fc6e6b5b12b452
darv melv <darvieuxmelvin@gmail.com>
2022-08-31 12:49:27 +0200

    problem when creating a new user-action

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -8,5 +8,7 @@ class ActionsController < ApplicationController
   end
   def show
     @action = Action.find(params[:id])
+    @user = current_user
+    @user_action = UserAction.new()
   end
 end
--- app/controllers/user_actions_controller.rb
+++ app/controllers/user_actions_controller.rb
@@ -10,4 +10,21 @@ class UserActionsController < ApplicationController
   def show
     @action = UserAction.joins(:action).find(params[:id])
   end
+
+  def create
+    @new_user_action = UserAction.new(get_params)
+    @new_user_action.status = 'unvalidated'
+    @new_user_action.user_occurences = 0
+    @new_user_action.user_id = current_user
+    if @new_user_action.save
+      redirect_to user_actions_path
+    else
+      raise
+    end
+  end
+
+
+  def get_params
+    params.require(:user_action).permit(:user_id, :action_id, :score, :category, :title, :status, :user_occurences)
+  end
 end
