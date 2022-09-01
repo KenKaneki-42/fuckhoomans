3517938cd8b1ff6794482bef9ac32d69f9917616
Kenkaneki-42 <sylvain.vandermeersch@gmail.com>
2022-09-01 11:37:13 +0200

    run migration schema update ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_31_140403) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_01_093147) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_31_140403) do
     t.integer "occurences", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "carbongain", default: 0
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
