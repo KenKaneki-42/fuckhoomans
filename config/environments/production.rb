770ffaa0a2fa5807584ab0a10c6f014e9589e593
Kenkaneki-42 <sylvain.vandermeersch@gmail.com>
2022-09-01 11:36:09 +0200

    add migration to add column carbongain in table actions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "fuckhoomans_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'https://fuckhoomans.herokuapp.com/'}
+  config.action_mailer.default_url_options = { host: 'https://fuckhoomans.herokuapp.com/' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
