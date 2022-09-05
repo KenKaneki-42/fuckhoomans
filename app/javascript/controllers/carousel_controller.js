77bcc0ec5460345a25be0d46bf1b9e5573c08c95
darv melv <darvieuxmelvin@gmail.com>
2022-09-05 07:52:41 +0200

    unfinished homework: responsive design, carousel

---
+++ app/javascript/controllers/carousel_controller.js
@@ -0,0 +1,12 @@
+
+
+// Connects to data-controller="carousel"
+export default class extends Carousel {
+  connect() {
+    super.connect()
+    console.log('Do what you want here.')
+
+    // The swiper instance.
+    this.swiper
+  }
+}
