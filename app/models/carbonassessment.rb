7e699a81b7f02ff6320c369a37eaadfe2c68805d
Kenkaneki-42 <sylvain.vandermeersch@gmail.com>
2022-09-07 14:40:21 +0200

    add validates on Carbonassessment and organise a little form

--- app/models/carbonassessment.rb
+++ app/models/carbonassessment.rb
@@ -3,4 +3,23 @@ class Carbonassessment < ApplicationRecord
 
 
   validates :bike, inclusion: 1..3
+  validates :bus, inclusion: 1..3
+  validates :plane, inclusion: 1..3
+  validates :car, inclusion: 1..3
+
+  validates :email, inclusion: 1..3
+  validates :streaming, inclusion: 1..3
+  validates :devise, inclusion: 1..3
+  validates :second_hand, inclusion: 1..3
+
+  validates :red_meat, inclusion: 1..3
+  validates :white_meat, inclusion: 1..3
+  validates :oil, inclusion: 1..3
+  validates :dairy, inclusion: 1..3
+
+  validates :house_size, inclusion: 1..3
+  validates :house_heated, inclusion: 1..3
+  validates :renewable_energy, inclusion: 1..3
+  validates :people_number, inclusion: 1..3
+
 end
